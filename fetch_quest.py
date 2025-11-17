7b9f29fc1be4d334fb395a0e7a7c653e5f6b9100
SamDavis64 <samdavis64@icloud.com>
2025-11-18 09:33:49 +1100

    added main algorithm as comments

---
+++ fetch_quest.py
@@ -0,0 +1,15 @@
+#BEGIN fetch_quest
+#	Get difficulty
+#	game_map, player = initialise_game(difficulty)
+#	running = True
+#	won = False
+#	WHILE running AND NOT won
+#		Print description of players current location
+#		Get command
+#		running = process_command(command, player, game_map)
+#		won = (number of items = NUM_ITEMS) AND (player.location = game_map.starting_location)
+#	ENDWHILE
+#	IF won THEN
+#		Print “Congratulations!”
+#	ENDIF
+#END fetch_quest
