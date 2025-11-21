38d1b51a95d5ddebe62ecee615a65af875ee9424
SamDavis64 <samdavis64@icloud.com>
2025-11-21 18:13:41 +1100

    add comments

--- fetch_quest.py
+++ fetch_quest.py
@@ -1,17 +1,35 @@
+NUM_ITEMS = 5
+NUM_LOCATIONS = 5
+
+def initialise_game(difficulty):
+    pass
+
+
+def process_command(command, player, game_map):
+    pass
+
 
 def fetch_quest():
-    difficulty = int(input("Enter difficulty level (1–100): "))
+    """
+    Initialises the game, then runs the Fetch Quest game loop.
+    Prompts the user for commands, updates the player state, and checks whether the win condition has been met.
+    """
+    difficulty = int(input("Enter difficulty level (1–100): ")) # convert text input to integer
     game_map, player = initialise_game(difficulty)
     running = True
     won = False
     while running and not won:
         player.location.describe()
-        command = input("What do you want to do? ")
+        command = input("What do you want to do? ") # TODO consider sanitising the input
         running = process_command(command, player, game_map)
-        won = (number of items = NUM_ITEMS) and (player.location = game_map.starting_location)
+        # Determine if the player has won:
+        # 1. They must collect all required items
+        # 2. They must return to the starting location
+        won = (len(player.inventory) == NUM_ITEMS) and (player.location == game_map.starting_location)
 
     if won:
-        print('Congratulation')
+        print('Congratulations')
+        # only other option is to quit which is dealt with by process_command and returns False
 
 if __name__ == "__main__":
     fetch_quest()
