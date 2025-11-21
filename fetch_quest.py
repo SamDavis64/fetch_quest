fecf544886464955e4326371cf630c0733840bad
SamDavis64 <samdavis64@icloud.com>
2025-11-21 13:29:21 +1100

    main program coded

--- fetch_quest.py
+++ fetch_quest.py
@@ -1,12 +1,4 @@
 
-def initialise_game(difficulty):
-    pass
-
-
-def process_command(command, player, game_map):
-    pass
-
-
 def fetch_quest():
     difficulty = int(input("Enter difficulty level (1–100): "))
     game_map, player = initialise_game(difficulty)
@@ -15,13 +7,11 @@ def fetch_quest():
     while running and not won:
         player.location.describe()
         command = input("What do you want to do? ")
-#		running = process_command(command, player, game_map)
-#		won = (number of items = NUM_ITEMS) AND (player.location = game_map.starting_location)
-#	ENDWHILE
-#	IF won THEN
-#		Print “Congratulations!”
-#	ENDIF
-# END fetch_quest
+        running = process_command(command, player, game_map)
+        won = (number of items = NUM_ITEMS) and (player.location = game_map.starting_location)
+
+    if won:
+        print('Congratulation')
 
 if __name__ == "__main__":
     fetch_quest()
