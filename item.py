42b3ad510deaf2150f92a5be394fa77166f0bc83
SamDavis64 <samdavis64@icloud.com>
2025-11-22 15:17:51 +1100

    Add item and location class files

---
+++ item.py
@@ -0,0 +1,10 @@
+# ----- Item Class -----
+class Item:
+    """Items are collected as the game is played."""
+    def __init__(self, name, description=""):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return self.name
+
---
+++ location.py
@@ -0,0 +1,26 @@
+# ----- Location Class -----
+class Location:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+        self.neighbors = {}
+
+    def connect(self, other, direction):
+        opposites = {'north': 'south', 'south': 'north',
+                     'east': 'west', 'west': 'east'}
+        self.neighbors[direction] = other
+        other.neighbors[opposites[direction]] = self
+
+    def describe(self):
+        print(f"\nYou are at {self.name}.")
+        if self.items:
+            print("You see the following items:")
+            for item in self.items:
+                print(f" - {item}")
+        else:
+            print("There is nothing of interest here.")
+        if self.neighbors:
+            print("Exits:", ", ".join(self.neighbors.keys()))
+        else:
+            print("There are no visible exits.")
+
